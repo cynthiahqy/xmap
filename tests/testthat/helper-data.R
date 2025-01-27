ac94716a3dbcfd39a5b83c9ef8f885af7bbf10cf
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2025-01-27 22:30:48 +1100

    add test objects multicol links and xmap

--- tests/testthat/helper-data.R
+++ tests/testthat/helper-data.R
@@ -6,3 +6,16 @@ simple_xmap <- as_xmap_tbl(
     simple_links,
     xcode, alphacode, weight
 )
+
+multicol_links <- simple_links |>
+    tidyr::separate_wider_position(
+        xcode,
+        c(x_letter = 1, x_numbers = 4)
+    )
+multicol_xmap <- xmap:::new_xmap_tbl(
+    list(
+        .from = multicol_links[c("x_letter", "x_numbers")],
+        .to = multicol_links["alphacode"],
+        .weight_by = multicol_links["weight"]
+    )
+)
