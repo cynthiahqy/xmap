ab74b031e27dbcf02deedc14e74eeeb0705cc245
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2026-08-12 00:29:38 +0200

    Add country-labelled weight-type visualisation to isiccomb split case
    Facet the weight-type breakdown by country_iso3c/name instead of raw
    country codes, and fix the country_lookup -> indstat_country_lookup
    rename left incomplete in data-raw/indstat.R (indstat_masked was
    missing country_iso3c/country_name as a result). Also fixes a
    masked_indstat/indstat_masked typo that broke split_links.

--- data-raw/indstat.R
+++ data-raw/indstat.R
@@ -1,4 +1,4 @@
-country_lookup <- 
+indstat_country_lookup <- 
   structure(list(code = c("076", "156", "276", "392", "840"), name = c("Brazil", 
 "China", "Germany", "Japan", "United States of America"), iso3c = c("BRA", 
 "CHN", "DEU", "JPN", "USA"), income_group = c("UM", "LM", "H", 
@@ -6,7 +6,8 @@ country_lookup <-
 
 indstat_masked <- 
   readr::read_csv("data-raw/indstat_rev3_masked_subset.csv") |>
-  dplyr::left_join(country_lookup[c("code", "iso3c", "name")], by = join_by(country == code)) |>
+  dplyr::left_join(indstat_country_lookup[c("code", "iso3c", "name")], by = join_by(country == code)) |>
   dplyr::rename(country_name = name, country_iso3c = iso3c)
 
 usethis::use_data(indstat_masked)
+usethis::use_data(indstat_country_lookup)
